b26b21a3e29c0ce70917dea548d547c486b37c1a
renan <renan.nic28@gmail.com>
2023-09-24 10:22:21 -0300

    update 24/09/2023

--- Load.py
+++ Load.py
@@ -2,8 +2,6 @@ import controlador_banco
 from Transformer_dados import TranformarDados
 
 
-# controlador_banco.update_resgistro(user[0], "mensagem_de_bem_estar", frase)
-
 def update_user():
     for user in TranformarDados().adicionar_frase():
         controlador_banco.update_resgistro(user[0], "mensagem_de_bem_estar", user[3])
